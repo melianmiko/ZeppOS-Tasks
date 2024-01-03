a3ee0e119d6678c5ea07c56c389bc9cf874fca2d
MelianMiko <support@mmk.pw>
2024-01-03 13:40:37 +0700

    Connect new translations

--- page/Translations.js
+++ page/Translations.js
@@ -1,11 +1,14 @@
 import { loadLocale } from "../lib/mmk/i18n";
 
-import {strings as russianStrings} from "./translations/ru-RU";
 import {strings as englishStrings} from "./translations/en-US";
+import {strings as russianStrings} from "./translations/ru-RU";
+import {strings as germanStrings} from "./translations/de-DE";
 import {strings as chineseStrings} from "./translations/zh-CN";
 import {strings as hungarianStrings} from "./translations/hu-HU";
+import {strings as italianStrings} from "./translations/it-IT";
 import {strings as frenchStrings} from "./translations/fr-FR";
 import {strings as spanishStrings} from "./translations/es-ES";
+import {strings as brazilianStrings} from "./translations/pt-BR";
 
 /**
  * Locale contents was split into separate files and saved into
@@ -18,7 +21,10 @@ import {strings as spanishStrings} from "./translations/es-ES";
 
 loadLocale("en-US", englishStrings);
 loadLocale("ru-RU", russianStrings);
+loadLocale("de-DE", germanStrings);
 loadLocale("zh-CN", chineseStrings);
 loadLocale("hu-HU", hungarianStrings);
+loadLocale("it-IT", italianStrings);
 loadLocale("fr-FR", frenchStrings);
 loadLocale("es-ES", spanishStrings);
+loadLocale("pt-BR", brazilianStrings);
